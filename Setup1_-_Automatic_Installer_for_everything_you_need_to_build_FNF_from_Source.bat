5004ff4df286bc541d6ac429e6a55a2212102fb4
MeguminBOT <discord.meguminbot@gmail.com>
2022-06-18 19:43:32 +0200

    Replace winget with curl
    This gives support for windows builds older than 1703

--- Setup1_-_Automatic_Installer_for_everything_you_need_to_build_FNF_from_Source.bat
+++ Setup1_-_Automatic_Installer_for_everything_you_need_to_build_FNF_from_Source.bat
@@ -1,17 +1,22 @@
 @echo off
-title FNF Automatic Build Environment Setup Script - Start
-echo AutisticLulu's updated version of the Setup.bat originally by Tuxsuper@GameBanana.
-echo Now installs EVERYTHING you need to compile Psych or Kade Engine. It's not fully automatic but almost!
-echo Additionally, it offers two optional steps to attempt making it easier for new people to edit the code.
 
+title FNF Automatic Build Environment Setup Script - Start
+echo AutisticLulu's updated version of the Setup.bat 
+echo originally created by Tuxsuper @ GameBanana.
+echo --
+echo Now installs EVERYTHING you need to compile Psych Engine or Kade Engine. It's not fully automatic but almost!
+echo --
+echo Additionally, it offers two optional steps to attempt making it easier for new people to edit and maintain the code.
+echo --
 echo Press any key to continue.
 pause >nul
 goto PromptGit
 
 :PromptGit
 cls
-title FNF Automatic Build Environment Setup Script - User action required - Prompt Git for Windows
+title FNF Automatic Build Environment Setup Script - Prompt Git for Windows
 echo *Git is required for future steps*
+echo --
 set /p menu="Would you like to automatically install/update Git for Windows? [Y/N]"
        if %menu%==Y goto InstallGit
        if %menu%==y goto InstallGit
@@ -20,28 +25,41 @@ set /p menu="Would you like to automatically install/update Git for Windows? [Y/
 
 :InstallGit 
 cls
-title FNF Automatic Build Environment Setup Script - Installing Git for Windows
+title FNF Automatic Build Environment Setup Script - Downloading Git for Windows
 curl -L -# -O https://github.com/git-for-windows/git/releases/download/v2.36.1.windows.1/Git-2.36.1-64-bit.exe
+title FNF Automatic Build Environment Setup Script - Installing Git for Windows
 Git-2.36.1-64-bit.exe
-title FNF Automatic Build Environment Setup Script - Installed Git
-echo Installed Git for Windows. Press any key to continue.
+del Git-2.36.1-64-bit.exe
+echo Deleting setup file...
+title FNF Automatic Build Environment Setup Script - Installed Git for Windows
+echo -- 
+echo Installed Git for Windows.
+echo -- 
+echo Press any key to continue.
 pause >nul
 goto PromptHaxe
 
 :SkipGit
 cls
 title FNF Automatic Build Environment Setup Script - Skipped Git
-echo Skipped Git for Windows. Press any key to continue.
+echo Skipped Git for Windows. 
+echo -- 
+echo Press any key to continue.
 pause >nul
 goto PromptHaxe
 
 :PromptHaxe
 cls
-title FNF Automatic Build Environment Setup Script - User action required - Haxe
-echo *Required*
-echo If building Psych Engine, press [P], this installs latest Haxe 4.2.5
+title FNF Automatic Build Environment Setup Script - Haxe
+echo *Haxe is required for future steps*
+echo --
+echo If building Psych Engine, press [P], this installs Haxe 4.2.5
 echo If building Kade Engine, press [K], this installs Haxe 4.1.5
-set /p menu="[P] for Psych, [K] for Kade, [1] if you are building Psych later, [2] if you are building Kade later. [P/K/1/2]"
+echo --
+echo If building Psych Engine but you want to skip Haxe for whatever reason, press [1]
+echo If building Kade Engine but you want to skip Haxe for whatever reason, press [2]
+echo --
+set /p menu="[P] for Psych, [K] for Kade, [1] Psych Skip, [2] Kade Skip. [P/K/1/2]"
        if %menu%==P goto InstallHaxePsych
        if %menu%==p goto InstallHaxePsych
        if %menu%==K goto InstallHaxeKade
@@ -51,48 +69,69 @@ set /p menu="[P] for Psych, [K] for Kade, [1] if you are building Psych later, [
 	   
 :InstallHaxePsych
 cls
-title FNF Automatic Build Environment Setup Script - Installing Haxe 4.2.5
+title FNF Automatic Build Environment Setup Script - Downloading Haxe 4.2.5
 curl -L -# -O https://github.com/HaxeFoundation/haxe/releases/download/4.2.5/haxe-4.2.5-win64.exe
+title FNF Automatic Build Environment Setup Script - Installing Haxe 4.2.5
 haxe-4.2.5-win64.exe
+del haxe-4.2.5-win64.exe
+echo Deleting setup file...
 title FNF Automatic Build Environment Setup Script - Installed Haxe 4.2.5
-echo Installed Haxe 4.2.5. Press any key to continue.
+echo --
+echo Installed Haxe 4.2.5.
+echo --
+echo Press any key to continue.
 pause >nul
 goto InstallHaxeLibsPsych
 
 :InstallHaxeKade
 cls
-title FNF Automatic Build Environment Setup Script - Installing Haxe 4.1.5
+title FNF Automatic Build Environment Setup Script - Downloading Haxe 4.1.5
 curl -L -# -O https://github.com/HaxeFoundation/haxe/releases/download/4.1.5/haxe-4.1.5-win64.exe
+title FNF Automatic Build Environment Setup Script - Installing Haxe 4.1.5
 haxe-4.1.5-win64.exe
+echo Deleting setup file...
+del haxe-4.1.5-win64.exe
 title FNF Automatic Build Environment Setup Script - Installed Haxe 4.1.5
-echo Installed Haxe 4.1.5. Press any key to continue.
+echo --
+echo Installed Haxe 4.1.5. 
+echo --
+echo Press any key to continue.
 pause >nul
 goto InstallHaxeLibsKade
 
 :SkipHaxePsych
 cls
 title FNF Automatic Build Environment Setup Script - Skipped Haxe 4.2.5
-echo Skipped Haxe 4.2.5. Press any key to continue.
+echo Skipped Haxe 4.2.5. 
+echo --
+echo NOTE: Next step will fail if you don't have Haxe installed.
+echo --
+echo Press any key to continue.
 pause >nul
 goto InstallHaxeLibsPsych
 
 :SkipHaxeKade
 cls
 title FNF Automatic Build Environment Setup Script - Skipped Haxe 4.2.5
-echo Skipped Haxe 4.1.5. Press any key to continue.
+echo Skipped Haxe 4.1.5.
+echo --
+echo NOTE: Next step will fail if you don't have Haxe installed.
+echo --
+echo Press any key to continue.
 pause >nul
 goto InstallHaxeLibsKade
 
 :InstallHaxeLibsPsych
 cls
-title FNF Automatic Build Environment Setup Script - User action required - HaxeLibs
+title FNF Automatic Build Environment Setup Script - HaxeLibs
 echo Press any key to install the required Haxe libraries /~500 MB.
+echo --
 echo They will be installed to C:/haxelib by default and will set itself to call haxelibs from here by default when compiling.
-echo *Required*
 pause >nul
 title FNF Automatic Build Environment Setup Script - Installing Haxe libraries
-echo Installing haxelib libraries...
+echo haxelib path is now C:/haxelib
 haxelib setup C:/haxelib
+echo Installing haxelib libraries...
 haxelib install lime
 haxelib install openfl
 haxelib --never install flixel
@@ -112,9 +151,13 @@ haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib install hxcpp > nul
 haxelib install hxcpp-debug-server
 title FNF Automatic Build Environment Setup Script - Verify Haxe libraries
+echo --
 echo Press any key to list all packages and verify that they got installed successfully.
+echo --
 pause >nul
+echo --
 haxelib list
+echo --
 title FNF Automatic Build Environment Setup Script - Installed Haxe libraries
 echo Press any key to continue.
 pause >nul
@@ -122,12 +165,14 @@ goto PromptVSCommunity
 
 :InstallHaxeLibsKade
 cls
-title FNF Automatic Build Environment Setup Script - User action required - HaxeLibs
+title FNF Automatic Build Environment Setup Script - HaxeLibs
 echo Press any key to install the required Haxe libraries /~500 MB.
+echo --
 echo They will be installed to C:/haxelib by default and will set itself to call haxelibs from here by default when compiling.
-echo *Required*
 pause >nul
 title FNF Automatic Build Environment Setup Script - Installing Haxe libraries
+echo haxelib path is now C:/haxelib
+haxelib setup C:/haxelib
 echo Installing haxelib libraries...
 haxelib setup C:/haxelib
 haxelib install lime
@@ -149,9 +194,13 @@ haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib install hxcpp > nul
 haxelib install hxcpp-debug-server
 title FNF Automatic Build Environment Setup Script - Verify Haxe libraries
+echo --
 echo Press any key to list all packages and verify that they got installed successfully.
+echo --
 pause >nul
+echo --
 haxelib list
+echo --
 title FNF Automatic Build Environment Setup Script - Installed Haxe libraries
 echo Press any key to continue.
 pause >nul
@@ -159,8 +208,9 @@ goto PromptVSCommunity
 
 :PromptVSCommunity
 cls
-title FNF Automatic Build Environment Setup Script - User action required - Visual Studio 2019
+title FNF Automatic Build Environment Setup Script - Visual Studio 2019
 echo *Required for compiling on Windows*
+echo --
 set /p menu="Would you like to install Visual Studio 2019 and the required components? /~5.5 GB [Y/N]"
        if %menu%==Y goto InstallVSCommunity
        if %menu%==y goto InstallVSCommunity
@@ -172,9 +222,12 @@ cls
 title FNF Automatic Build Environment Setup Script - Installing Visual Studio Community
 curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
 vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p --norestart
+echo Deleting setup file...
 del vs_Community.exe
 title FNF Automatic Build Environment Setup Script - Installed Visual Studio Community
-echo Installed Visual Studio 2019, MSVC v142 - VS 2019 C++ x64/x86 build tools and Windows 10 (19041) SDK. 
+echo --
+echo Installed Visual Studio 2019, MSVC v142 - VS 2019 C++ x64/x86 build tools and Windows 10 (19041) SDK.
+echo --
 echo Press any key to continue.
 pause >nul
 goto PromptVSCode
@@ -182,16 +235,19 @@ goto PromptVSCode
 :SkipVSCommunity
 cls
 title FNF Automatic Build Environment Setup Script - Skipped Visual Studio 2019
-echo Skipped Visual Studio Community 2019. Press any key to continue
+echo Skipped Visual Studio Community 2019. 
+echo --
+echo Press any key to continue
 pause >nul
 goto PromptVSCode
 
 :PromptVSCode
 cls
-title FNF Automatic Build Environment Setup Script - User action required - Visual Studio Code IDE
-echo You need Windows 11 or Windows 10 version 1709 or newer to automatically install this, otherwise you have to download it manually.
+title FNF Automatic Build Environment Setup Script - Visual Studio Code IDE
 echo Visual Studio Code is an easy to use IDE that will make your life editing code easier.
-echo *Optional*
+echo --
+echo *Optional but recommended*
+echo --
 set /p menu="Would you like to install Visual Studio Code and with some extensions to it? /~300 MB [Y/N]"
        if %menu%==Y goto InstallVSCode
        if %menu%==y goto InstallVSCode
@@ -200,27 +256,49 @@ set /p menu="Would you like to install Visual Studio Code and with some extensio
 
 :InstallVSCode
 cls
-title FNF Automatic Build Environment Setup Script - Install Visual Studio Code IDE.
-winget install -e --id Microsoft.VisualStudioCode --source winget
-https://az764295.vo.msecnd.net/stable/30d9c6cd9483b2cc586687151bcbcd635f373630/VSCodeUserSetup-x64-1.68.1.exe
-title FNF Automatic Build Environment Setup Script - Installed Visual Studio Code IDE.
-echo Press any key to continue.
+title FNF Automatic Build Environment Setup Script - Downloading Visual Studio Code IDE
+curl -L -# -O https://az764295.vo.msecnd.net/stable/30d9c6cd9483b2cc586687151bcbcd635f373630/VSCodeUserSetup-x64-1.68.1.exe
+title FNF Automatic Build Environment Setup Script - Installing Visual Studio Code IDE
+VSCodeUserSetup-x64-1.68.1.exe
+echo Deleting setup file...
+del VSCodeUserSetup-x64-1.68.1.exe
+title FNF Automatic Build Environment Setup Script - Installed Visual Studio Code IDE
+echo --
+echo Installed Visual Studio Code IDE.
+echo --
+echo Press any key to install the required extensions.
+echo --
 pause >nul
+title FNF Automatic Build Environment Setup Script - Downloading VScode Extension Pack
+curl -L -# -O https://github.com/MeguminBOT/FNF-Source-Automatic-Setup/raw/main/curl/AutisticLulus-VSCode-Extension-Pack.exe
+title FNF Automatic Build Environment Setup Script - Installing VScode Extension Pack
+AutisticLulus-VSCode-Extension-Pack.exe
+echo Deleting setup file...
+del AutisticLulus-VSCode-Extension-Pack.exe
+title FNF Automatic Build Environment Setup Script - Installed VScode Extension Pack
+echo --
+echo Installed Visual Studio Code Extension Pack.
+echo --
+echo Press any key to to continue.
 goto PromptGithubDesktop
 
 :SkipVSCode
 cls
 title FNF Automatic Build Environment Setup Script - Skipped Visual Studio Code IDE
-echo Skipped Visual Studio Code. Press any key to continue
+echo Skipped Visual Studio Code and extension pack. 
+echo --
+echo Press any key to continue
 pause >nul
 goto PromptGithubDesktop
 
 :PromptGithubDesktop
 cls
-title FNF Automatic Build Environment Setup Script - User action required - Github Desktop
-echo You need Windows 11 or Windows 10 version 1709 or newer to automatically install this, otherwise you have to download it manually.
-echo GitHub Desktop makes it easier to track what changes you've made and can revert changes in one button press if needed. It can be used locally only too if desired.
-echo *Optional*
+title FNF Automatic Build Environment Setup Script - Github Desktop
+echo GitHub Desktop makes it easier to track what changes you've made and can revert changes in one button press if needed. 
+echo It can be used locally only too if desired.
+echo --
+echo *Optional but recommended*
+echo --
 set /p menu="Would you like to install GitHub Desktop? /~100 MB [Y/N]"
        if %menu%==Y goto InstallGithubDesktop
        if %menu%==y goto InstallGithubDesktop
@@ -229,17 +307,26 @@ set /p menu="Would you like to install GitHub Desktop? /~100 MB [Y/N]"
 
 :InstallGithubDesktop
 cls
+title FNF Automatic Build Environment Setup Script - Downloading GitHub Desktop App
+curl -L -# -O https://desktop.githubusercontent.com/github-desktop/releases/3.0.2-11fd584c/GitHubDesktopSetup-x64.exe
 title FNF Automatic Build Environment Setup Script - Installing GitHub Desktop App
-winget install -e --id GitHub.GitHubDesktop --source winget
+GitHubDesktopSetup-x64.exe
+echo Deleting setup file..
+del GitHubDesktopSetup-x64.exe
 title FNF Automatic Build Environment Setup Script - Installed GitHub Desktop App
-echo Installed GitHub Desktop App. Press any key to continue.
+echo --
+echo Installed GitHub Desktop App. 
+echo --
+echo Press any key to continue.
 pause >nul
 goto Information
 
 :SkipGithubDesktop
 cls   
 title FNF Automatic Build Environment Setup Script - Skipped GitHub Desktop App
-echo Skipped GitHub Desktop App. Press any key to continue
+echo Skipped GitHub Desktop App. 
+echo --
+echo Press any key to continue
 pause >nul
 goto Information
 
@@ -247,13 +334,18 @@ goto Information
 cls
 title FNF Automatic Build Environment Setup Script - Information
 echo Now everything that you need should be installed.
+echo --
 echo Don't use the command "lime test windows" to build, it's outdated and doesn't actually properly compile the game.
 echo Instead, use "haxelib run lime build windows" to properly build the game.
-echo You can now run the Setup2.bat script to automatically download latest source code and test compile after rebooting your PC *REQUIRED*
+echo --
+echo Your PC will need to reboot before running any of the optional scripts.
+echo --
+echo The Setup2.bat script can automatically 
+echo download latest stable versions of Psych/Kade source code or custom github repo for you.
+echo It also offers to run a compile test to make sure everything is working.
+echo --
 echo Save any work you have open, then press any key to reboot.
 pause >nul
 cls
-echo Shutting down in 10 seconds.
+echo Automatically Shutting down in 10 seconds.
 SHUTDOWN -r -t 10
-
-
