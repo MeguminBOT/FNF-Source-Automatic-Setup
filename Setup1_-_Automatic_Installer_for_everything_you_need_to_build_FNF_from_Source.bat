0f46f9882d38914a643ba12ae93aaae13ba467b4
MeguminBOT <discord.meguminbot@gmail.com>
2023-05-13 16:52:55 +0200

    Update Setup1_-_Automatic_Installer_for_everything_you_need_to_build_FNF_from_Source.bat

--- Setup1_-_Automatic_Installer_for_everything_you_need_to_build_FNF_from_Source.bat
+++ Setup1_-_Automatic_Installer_for_everything_you_need_to_build_FNF_from_Source.bat
@@ -132,24 +132,9 @@ title FNF Automatic Build Environment Setup Script - Installing Haxe libraries
 echo haxelib path is now C:/haxelib
 haxelib setup C:/haxelib
 echo Installing haxelib libraries...
-haxelib install lime
-haxelib install openfl
-haxelib --never install flixel
-haxelib run lime setup flixel
-haxelib run lime setup 
-haxelib install flixel-tools
-haxelib install flixel-ui
-haxelib install flixel-addons
-haxelib install tjson
-haxelib install hxjsonast
-haxelib install hscript
-haxelib install hxCodec
-haxelib git polymod https://github.com/larsiusprime/polymod.git
-haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
-haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
-haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib install hxcpp > nul
-haxelib install hxcpp-debug-server
+haxelib install hmm
+haxelib run hmm install
+
 title FNF Automatic Build Environment Setup Script - Verify Haxe libraries
 echo --
 echo Press any key to list all packages and verify that they got installed successfully.
